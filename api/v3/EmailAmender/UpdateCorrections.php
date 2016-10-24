aa760c1af7d26fde010e79b1e9162a020268ec16
John Kirk <john@civifirst.com>
2016-10-24 12:51:51 +0000

    Tidied API file.

--- api/v3/EmailAmender/UpdateCorrections.php
+++ api/v3/EmailAmender/UpdateCorrections.php
@@ -10,7 +10,7 @@
  */
 
 function _civicrm_api3_email_amender_update_corrections_spec(&$spec) {
-//  $spec['magicword']['api.required'] = 1;
+  $spec['domain_level']['api.required'] = 1;
 }
 
 /**
@@ -24,13 +24,8 @@ function _civicrm_api3_email_amender_update_corrections_spec(&$spec) {
  */
 function civicrm_api3_email_amender_update_corrections($params) {
 
-  if (!array_key_exists('domain_level', $params)){
-    throw new API_Exception(/*errorMessage*/'Missing array key domain level', /*errorCode*/ 1);
-  }
-
-  $aEscapedCorrections = array();
   $sCivicrmSettingsKey = "";
-  
+
   switch($params['domain_level']){
     case 'top_level_domain':
       $sCivicrmSettingsKey = 'emailamender.top_level_domain_corrections';
@@ -45,10 +40,12 @@ function civicrm_api3_email_amender_update_corrections($params) {
       break;
 
     default:
-      return civicrm_api3_create_error(ts("Couldn't update settings - invalid domain level"), 
-        array('error_code' => '2', 'field' => 'domain_level')
+      return civicrm_api3_create_error(ts("Couldn't update settings - invalid domain level"),
+        array('error_code' => '1', 'field' => 'domain_level')
       );
   }
+
+  $aEscapedCorrections = array();
 
   foreach( $params['correction_keys'] as $index => $correctionKey ){
     $aEscapedCorrections[CRM_Core_DAO::escapeString(correctionKey)] = CRM_Core_DAO::escapeString($params['correction_values'][$index]);
@@ -56,8 +53,6 @@ function civicrm_api3_email_amender_update_corrections($params) {
 
   CRM_Core_BAO_Setting::setItem($aEscapedCorrections, 'uk.org.futurefirst.networks.emailamender', $sCivicrmSettingsKey);
 
-  // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-  // return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  return civicrm_api3_create_success($returnValues, $params, 'EmailAmender', 'UpdateCorrections');
+  return civicrm_api3_create_success(array(), $params, 'EmailAmender', 'UpdateCorrections');
 }
 
