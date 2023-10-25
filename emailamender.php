7a00ea23774544ef8be213060ca597a997d5a688
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-10-25 17:52:49 +1300

    Move Navigation & Activity Type to managed entities

--- emailamender.php
+++ emailamender.php
@@ -12,35 +12,10 @@ function emailamender_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_install().
- */
-function emailamender_civicrm_install() {
-  CRM_Core_BAO_OptionValue::ensureOptionValueExists([
-    'label'        => E::ts('Corrected Email Address'),
-    'name'         => 'corrected_email_address',
-    'weight'       => '1',
-    'description'  => 'Automatically corrected emails (by the Email Address Corrector extension).',
-    'option_group_id' => 'activity_type',
-    'filter' => 1,
-  ]);
-  return _emailamender_civix_civicrm_install();
-}
-
-/**
  * Implements hook_civicrm_uninstall().
  */
 function emailamender_civicrm_uninstall() {
-
   CRM_Core_DAO::executeQuery("DELETE FROM civicrm_setting WHERE name LIKE 'emailamender%'");
-
-  return;
-}
-
-/**
- * Implements hook_civicrm_enable().
- */
-function emailamender_civicrm_enable() {
-  return _emailamender_civix_civicrm_enable();
 }
 
 /**
@@ -66,42 +41,14 @@ function emailamender_civicrm_emailProcessorContact($email, $contactID, &$result
 }
 
 /**
- * civicrm_civicrm_navigationMenu
- *
- * implementation of civicrm_civicrm_navigationMenu
- *
- */
-function emailamender_civicrm_navigationMenu(&$params) {
-  $sAdministerMenuId = CRM_Core_DAO::getFieldValue('CRM_Core_BAO_Navigation', 'Administer', 'id', 'name');
-  $sSystemSettingsMenuId = CRM_Core_DAO::getFieldValue('CRM_Core_BAO_Navigation', 'System Settings', 'id', 'name');
-
-  //  Get the maximum key of $params
-  $maxKey = max(array_keys($params));
-
-  $params[$sAdministerMenuId]['child'][$sSystemSettingsMenuId]['child'][$maxKey + 1] = array(
-    'attributes' => array(
-      'label'      => 'Email Address Corrector Settings',
-      'name'       => 'EmailAmenderSettings',
-      'url'        => 'civicrm/emailamendersettings',
-      'permission' => NULL,
-      'operator'   => NULL,
-      'separator'  => NULL,
-      'parentID'   => $sSystemSettingsMenuId,
-      'navID'      => $maxKey + 1,
-      'active'     => 1,
-    ),
-  );
-}
-
-/**
  * Implements hook_civicrm_searchTasks().
  */
 function emailamender_civicrm_searchTasks($objectType, &$tasks) {
   if ($objectType === 'contact') {
-    $tasks[] = array(
+    $tasks[] = [
       'title'  => ts('Email - correct email addresses'),
       'class'  => 'CRM_Emailamender_Form_Task_Correctemailaddresses',
       'result' => TRUE,
-    );
+    ];
   }
 }
